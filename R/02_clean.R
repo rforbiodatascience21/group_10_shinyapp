98c4c4b87064a0c975af108970da53930d2e06b9
lineroager <linero@dtu.dk>
2021-04-12 07:54:46 +0000

    Added 02_clean.R and 03_augment.R files.

---
+++ R/02_clean.R
@@ -0,0 +1,25 @@
+# Clear workspace ---------------------------------------------------------
+rm(list = ls())
+
+
+# Load libraries ----------------------------------------------------------
+library("tidyverse")
+
+
+# Define functions --------------------------------------------------------
+#source(file = "R/99_project_functions.R")
+
+
+# Load data ---------------------------------------------------------------
+gravier_x <- read_tsv(file = "data/01_gravier_x.tsv.gz")
+gravier_y <- read_tsv(file = "data/01_gravier_y.tsv.gz")
+
+
+# Wrangle data ------------------------------------------------------------
+gravier_data <- bind_cols(gravier_x,
+                          gravier_y)
+
+
+# Write data --------------------------------------------------------------
+write_tsv(x = gravier_data,
+          path = "data/02_gravier_data.tsv.gz")
---
+++ R/03_augment.R
@@ -0,0 +1,26 @@
+# Clear workspace ---------------------------------------------------------
+rm(list = ls())
+
+
+# Load libraries ----------------------------------------------------------
+library("tidyverse")
+
+
+# Define functions --------------------------------------------------------
+#source(file = "R/99_project_functions.R")
+
+
+# Load data ---------------------------------------------------------------
+gravier_data <- read_tsv(file = "data/02_gravier_data.tsv.gz")
+
+
+# Wrangle data ------------------------------------------------------------
+gravier_data_outcome <- gravier_data %>% 
+  mutate(outcome = case_when(value == "good" ~0,
+                             value == "poor" ~1)) %>% 
+  select(-value)
+
+
+# Write data --------------------------------------------------------------
+write_tsv(x = gravier_data_outcome,
+          path = "data/03_gravier_data_outcome.tsv.gz")
