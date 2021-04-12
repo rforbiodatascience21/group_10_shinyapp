efd7e4c00b33a421e2541177c86c035496e6cab2
RasDeh <rasdeh@dtu.dk>
2021-04-12 09:57:17 +0000

    Changes

--- RasDeh_app/server.R
+++ RasDeh_app/server.R
@@ -1,30 +1,30 @@
 library(shiny)
 library(tidyverse)
 
+gravier_augmented <- read_tsv(file = "RasDeh_app/03_gravier_data_outcome.tsv.gz")
+dataset <- gravier_augmented
+
 function(input, output) {
-    
-    dataset <- reactive({
-        diamonds[sample(nrow(diamonds), input$sampleSize),]
-    })
     
     output$plot <- renderPlot({
         
-        p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) + geom_point()
+        p <- dataset %>% 
+             ggplot(mapping = aes(x=input$x, y = input$y))+
+             geom_point()+
+             geom_smooth(method="lm")
         
-        if (input$color != 'None')
-            p <- p + aes_string(color=input$color)
+        #if (input$color != 'None')
+          #  p <- p + aes_string(color=input$color)
         
-        facets <- paste(input$facet_row, '~', input$facet_col)
-        if (facets != '. ~ .')
-            p <- p + facet_grid(facets)
+        #facets <- paste(input$facet_row, '~', input$facet_col)
+        #if (facets != '. ~ .')
+           #p <- p + facet_grid(facets)
         
-        if (input$jitter)
-            p <- p + geom_jitter()
-        if (input$smooth)
-            p <- p + geom_smooth()
+        #if (input$jitter)
+            #p <- p + geom_jitter()
         
         print(p)
         
-    }, height=700)
+    })
     
 }
--- RasDeh_app/ui.R
+++ RasDeh_app/ui.R
@@ -1,26 +1,30 @@
 library(shiny)
 library(tidyverse)
+library(broom)
+library(cowplot)
 
-dataset <- diamonds
+gravier_augmented <- read_tsv(file = "RasDeh_app/03_gravier_data_outcome.tsv.gz")
+
+dataset <- gravier_augmented
 
 fluidPage(
   
-  titlePanel("Diamonds Explorer"),
+  titlePanel("Gene Explorer"),
   
   sidebarPanel(
     
-    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
-                value=min(1000, nrow(dataset)), step=500, round=0),
+   # sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
+               # value=min(1000, nrow(dataset)), step=500, round=0),
     
     selectInput('x', 'X', names(dataset)),
-    selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
-    selectInput('color', 'Color', c('None', names(dataset))),
+    selectInput('y', 'Y', names(dataset)),
+    #selectInput('color', 'Color', c('None', names(dataset))),
     
-    checkboxInput('jitter', 'Jitter'),
-    checkboxInput('smooth', 'Smooth'),
+    #checkboxInput('jitter', 'Jitter'),
+    #checkboxInput('smooth', 'Smooth'),
     
-    selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
-    selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
+    #selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
+    #selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
   ),
   
   mainPanel(
